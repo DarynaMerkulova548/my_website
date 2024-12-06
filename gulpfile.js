ab112001ac951f412494dc2ecd9138488eae01c4
DarynaMerkulova548 <darinamerkulova984@gmail.com>
2024-12-06 11:31:04 +0200

    final_of_the_final

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,56 @@
 import gulp from 'gulp';
 import zip from 'gulp-zip';
-import tar from 'gulp-tar';
-import gzip from 'gulp-gzip';
+// const tar = require ('gulp-tar');
+// const gzip = require ('gulp-gzip');
+import * as sass from 'sass';
+import gulpSass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
 
-const paths = {
-    files: './dist/**/*',
-    debugArchive: './archives/debug.zip',
-    productionArchive: './archives/production.tar.gz',
+const sassCompile = gulpSass(sass);
+let options = {
+    sass_path: './css/',
+    css_path: './css/'
 };
 
-export const createDebugArchive = () => {
-    return gulp.src(paths.files)
-        .pipe(zip('debug.zip'))
-        .pipe(gulp.dest('./archives'));
-};
+let scssFiles = [
+    options.sass_path + '**/*.scss',
+    '!' + options.sass_path + '**/_*.scss',
+];
+gulp.task('sass.min', () => {
+    gulp.src(scssFiles)
+    .pipe(sassCompile())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(options.css_path))
+});
+gulp.task('sass', () => {
+    gulp.src(scssFiles)
+    .pipe(sassCompile())
+    .pipe(gulp.dest(options.css_path))
+});
 
-export const createProductionArchive = () => {
-    return gulp.src(paths.files)
-        .pipe(tar('production.tar'))
-        .pipe(gzip())
-        .pipe(gulp.dest('./archives'));
-};
+gulp.task('zip', () => {
+    gulp.src ( [ 
+        '*.html',
+        'css/*.css',
+        'img/*.jpg',
+        'img/*.png',
+    ], {base:'./'})
+    .pipe(zip('debug.zip'))
+    .pipe(gulp.dest('./dist'))
+})
 
-export default gulp.series(createDebugArchive, createProductionArchive);
+
+// export const createDebugArchive = () => {
+//     return gulp.src(paths.files)
+//         .pipe(zip('debug.zip'))
+//         .pipe(gulp.dest('./archives'));
+// };
+
+// export const createProductionArchive = () => {
+//     return gulp.src(paths.files)
+//         .pipe(tar('production.tar'))
+//         .pipe(gzip())
+//         .pipe(gulp.dest('./archives'));
+// };
+
+// export default gulp.series(createDebugArchive, createProductionArchive);
